679137806682070d081cf82da5f27d46cd7fb9f3
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-04-16 19:28:36 +0300

    Make base folder structure for project

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import './globals.css';
+import Header from '@/components/Header/Header';
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
