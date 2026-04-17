d8957b368c2bff956189701352939599e2270559
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-04-17 19:07:48 +0300

    Make markup and css for header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,27 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import css from './Header.module.css';
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={css.header}>
+      <Link className={css.logo} href="/">
+        <Image
+          alt="TravelTrucks logo"
+          src="/logo.svg"
+          height={16}
+          width={136}
+        />
+      </Link>
+
+      <nav className={css.nav}>
+        <Link className={css.link} href="/">
+          Home
+        </Link>
+        <Link className={css.link} href="/catalog">
+          Catalog
+        </Link>
+      </nav>
+    </header>
+  );
 }
