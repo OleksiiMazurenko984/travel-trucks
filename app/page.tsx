490b70e69185191c925145b6665dd002db614ca8
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-04-18 18:16:10 +0300

    Add markup and styles for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+import css from './page.module.css';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section className={css.hero}>
+      <div className={css.overlay}>
+        <div className={css.content}>
+          <h1 className={css.title}>Campers of your dreams</h1>
+          <p className={css.subtitle}>
+            You can find everything you want in our catalog
+          </p>
+          <Link className={css.view} href="/catalog">
+            View Now
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }
